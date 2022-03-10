b273053ecdfde2bfb254b4af78fbd3a4e3047fcc
ialexanderbrito <xanderzinho26@gmail.com>
2022-03-10 18:05:35 -0300

    🚚 Adicionado rota de salvos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import { Coin } from 'pages/Coin';
 import { Home } from 'pages/Home';
 import { NotFound } from 'pages/NotFound';
+import { Saved } from 'pages/Saved';
 
 export function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/coin/:id" element={<Coin />} />
+      <Route path="/save" element={<Saved />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
